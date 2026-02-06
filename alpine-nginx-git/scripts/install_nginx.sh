5d34b9afe397fb38c842920d50fe1c9cc6e016bd
Jérôme HELLARD <hellard.jerome@gmail.com>
2026-02-06 15:03:12 +0100

    Update install_nginx.sh

--- alpine-nginx-git/scripts/install_nginx.sh
+++ alpine-nginx-git/scripts/install_nginx.sh
@@ -1,12 +1,21 @@
-#!/bin/sh
-
-apk update
-
-apk add nginx
-
-rc-update add nginx default
-
-rc-service nginx start
-
-rc-service nginx status
-
+#!/bin/bash
+set -e  
+sudo apt update -y
+sudo apt install -y nginx
+sudo systemctl enable nginx
+sudo systemctl start nginx
+# Création d'une page HTML 
+HOSTNAME=$(hostname)
+sudo tee /var/www/html/index.html > /dev/null <<EOF
+<!DOCTYPE html>
+<html>
+<head>
+  <title>$HOSTNAME</title>
+</head>
+<body>
+  <h1>Serveur : $HOSTNAME</h1>
+  </body>
+</html>
+EOF
+sudo systemctl status nginx --no-pager
+echo "Installation Nginx terminée"
