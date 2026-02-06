709a38398d3b67c80afd3f2b31e97779a5c78abf
Jérôme HELLARD <hellard.jerome@gmail.com>
2026-02-06 13:23:55 +0100

    Update deploy_vm.sh

--- alpine-nginx-git/scripts/deploy_vm.sh
+++ alpine-nginx-git/scripts/deploy_vm.sh
@@ -1,1 +1,15 @@
+#!/bin/sh
+set -e
+
+VM_NAME="vm-alpine-nginx"
+IMAGE="Alpine-3.21"
+FLAVOR="m1.small"
+NETWORK="LAN-LABO"
+
+openstack server create \
+  --image "$IMAGE" \
+  --flavor "$FLAVOR" \
+  --network "$NETWORK" \
+  --user-data scripts/cloud-init.yaml \
+  "$VM_NAME"
 
