4ec35a46323e9da31311b7817d36a088d9da0f30
Jérôme HELLARD <hellard.jerome@gmail.com>
2026-02-06 16:07:08 +0100

    Update deploy_vm.sh

--- alpine-nginx-git/scripts/deploy_vm.sh
+++ alpine-nginx-git/scripts/deploy_vm.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 # Vérification des paramètres
 if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <nom_de_la_vm>"
+  echo "Usage: $0 VM_NAME"
   exit 1
 fi
+
 VM_NAME=$1
 IMAGE="alpine"
 FLAVOR="m1.tiny"
 NETWORK="LAN-LABO"
-KEY_NAME="default"
-# Vérifie si la VM existe déjà
+USER_DATA="scripts/cloud-init.yaml
+
 if openstack server list | grep -q "$VM_NAME"; then
   echo "La VM existe déjà !"
   exit 0
 fi
-# Création de la VM
+
 openstack server create \
   --image "$IMAGE" \
   --flavor "$FLAVOR" \
   --network "$NETWORK" \
+  --user-data "$USER_DATA" \
   "$VM_NAME"
-# Attente que la VM soit active et récupération de l'IP
+
 openstack server wait "$VM_NAME"
-IP=$(openstack server show "$VM_NAME" \
-     -f value -c addresses | cut -d= -f2)
+IP=$(openstack server show "$VM_NAME" -f value -c addresses | grep -oP '\d+\.\d+\.\d+\.\d+')
+
 echo "La VM $VM_NAME est déployée avec IP : $IP"
